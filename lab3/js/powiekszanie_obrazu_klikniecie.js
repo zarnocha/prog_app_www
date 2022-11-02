f214f9d55d5f509913edbf4bbfffc25bf59d5fc7
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2022-11-02 23:55:47 +0100

    lab3 - update jquery file

--- lab3/js/powiekszanie_obrazu_klikniecie.js
+++ lab3/js/powiekszanie_obrazu_klikniecie.js
@@ -1,4 +1,7 @@
 var naglowek_size = $(document.getElementsByClassName("naglowek")).css("fontSize");
+naglowek_size = parseFloat(naglowek_size);
+var oryginalna_szerokosc = parseFloat($(document.getElementsByClassName("logo")).css('width'));
+var oryginalna_wysokosc = parseFloat($(document.getElementsByClassName("logo")).css('height'));
 
 ($(document.getElementsByClassName('logo')).on("click", function() {
     if (!$(this).is(":animated")) 
@@ -7,15 +10,23 @@ var naglowek_size = $(document.getElementsByClassName("naglowek")).css("fontSize
             width: "-=" - 100,
             height: "+=" + 10,
             duration: 3000,
-        })
-
-        naglowek_size = parseFloat(naglowek_size)
-        if (naglowek_size < '145')
+        });
+        
+        if (naglowek_size < 145)
         {
-            $(document.getElementsByClassName("naglowek")).css("fontSize", "+=" + 5),
-            $(document.getElementsByClassName("dopisek")).css("fontSize", "+=" + 5),
-            $(document.getElementById("football_o")).css({width: "+=" + 5}),
-            naglowek_size += 5
+            $(document.getElementsByClassName("naglowek")).css("fontSize", "+=" + 5);
+            $(document.getElementsByClassName("dopisek")).css("fontSize", "+=" + 5);
+            $(document.getElementById("football_o")).css({width: "+=" + 5});
+            naglowek_size += 5;
+        }
+        else if (naglowek_size >= 145) {
+            console.log("test2")
+            $(document.getElementsByClassName("naglowek")).css("fontSize", "-=" + 75);
+            $(document.getElementsByClassName("dopisek")).css("fontSize", "-=" + 75);
+            $(document.getElementById("football_o")).css("width", "-=" + 75);
+            $(document.getElementsByClassName('logo')).height(oryginalna_wysokosc);
+            $(document.getElementsByClassName('logo')).width(oryginalna_szerokosc);
+            naglowek_size = 70;
         }
     }
 }
