8a76c5e06200770978aa05fbe46b324058b082a1
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2022-11-24 15:56:20 +0100

    lab7 - adding lab7

---
+++ lab7/php/zmienne.php
@@ -0,0 +1,9 @@
+<?php
+    $kierunek = 'Informatyka stacjonarnie';
+    $specjalizacja = 'Inżynieria Systemów Informatycznych';
+    $rok = 'III';
+    $imie = 'Artur';
+    $nazwisko = 'Żarnoch';
+    $wiek = 22;
+    $plec = 'mężczyzna';
+?>
